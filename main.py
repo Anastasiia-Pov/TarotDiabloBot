31a626e881346cd110bb1fc51ed7ee68add208e9
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-09-06 13:52:07 +0300

    updated

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from aiogram import Bot, Dispatcher
-
 import logging
 import asyncio
-from config import TOKEN
+from config import token
 from app.handlers import router
 # from database.models import async_main
 
 
 async def main():
     # await async_main()
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=token)
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
@@ -19,5 +18,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
-    except KeyboardInterrupt:
-        print('Exit')
+    except (KeyboardInterrupt, SystemExit):
+        print('Bot stopped')
